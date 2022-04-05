66048003a5a6071da45c2ff02c0118f32718bf00
Daniel Pahima <dpahima98@gmail.com>
2022-04-06 02:46:07 +0300

    add script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#docker pull 100570/my-app-java:1.0.1
+docker pull 100570/my-app-java:1.0.1
 DockerImage=`docker images | grep my-app-java | awk '{print $3}'`
-#docer run -d -it $DockerImage /bin/bash
+docer run -d -it $DockerImage /bin/bash
 DockerID=`docker ps | grep $DockerImage | awk '{print $1}'`
 docker exec -it $DockerID /bin/bash
 
